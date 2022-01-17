2bc3cb1e42182d885353b3f116dd5a9b04e2a553
Sander Ronde <awsdfgvhbjn@gmail.com>
2022-01-17 16:03:30 +0100

    add dev build

--- src/lib/util/dev.ts
+++ src/lib/util/dev.ts
@@ -1,4 +1,5 @@
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext, ExtensionMode } from 'vscode';
+import { logDev } from './log';
 
 let context: ExtensionContext | null = null;
 export function setDevContext(ctx: ExtensionContext): void {
@@ -6,26 +7,29 @@ export function setDevContext(ctx: ExtensionContext): void {
 }
 
 // Dev
+const IS_DEV_OVERRIDE: boolean | null = null;
 export function isDev(): boolean {
-	return true;
-	// if (!context) {
-	// 	// Use `logDev` to prevent an infinite loop
-	// 	logDev(
-	// 		'isDev called before context was set, returning false. Please flag this issue'
-	// 	);
-	// 	return false;
-	// }
-	// if (IS_DEV_OVERRIDE !== null) {
-	// 	if (context.extensionMode === ExtensionMode.Production) {
-	// 		// Use `logDev` to prevent an infinite loop
-	// 		logDev(
-	// 			'isDev override set in prod build (or context not set at call time), assuming prod mode. Please flag this issue'
-	// 		);
-	// 		return false;
-	// 	}
-	// 	return IS_DEV_OVERRIDE;
-	// }
-	// return context.extensionMode === ExtensionMode.Development;
+	if (process.env.DEV_OVERRIDE) {
+		return true;
+	}
+	if (!context) {
+		// Use `logDev` to prevent an infinite loop
+		logDev(
+			'isDev called before context was set, returning false. Please flag this issue'
+		);
+		return false;
+	}
+	if (IS_DEV_OVERRIDE !== null) {
+		if (context.extensionMode === ExtensionMode.Production) {
+			// Use `logDev` to prevent an infinite loop
+			logDev(
+				'isDev override set in prod build (or context not set at call time), assuming prod mode. Please flag this issue'
+			);
+			return false;
+		}
+		return IS_DEV_OVERRIDE;
+	}
+	return context.extensionMode === ExtensionMode.Development;
 }
 
 /**

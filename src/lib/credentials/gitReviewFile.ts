fc46adbf8aac947ad09104aacb9862bfe25818f1
Sander Ronde <awsdfgvhbjn@gmail.com>
2025-03-21 15:10:58 +0100

    Infer host and project from git remote (#91) (#92)
    * Infer host and project from git remote (#91)
    
    * Fix regex
    
    ---------
    
    Co-authored-by: Sander Ronde <sanderronde@crisp.nl>

--- src/lib/credentials/gitReviewFile.ts
+++ src/lib/credentials/gitReviewFile.ts
@@ -1,5 +1,6 @@
 import { Repository } from '../../types/vscode-extension-git';
 import { createCacheWrapper } from '../util/cache';
+import { tryExecAsync } from '../git/gitCLI';
 import { Uri, workspace } from 'vscode';
 import { log } from '../util/log';
 
@@ -24,6 +25,50 @@ export const DEFAULT_GIT_REVIEW_FILE: Required<OptionalGitReviewProperties> = {
 	port: '29418',
 };
 
+async function inferHostAndProject(
+	gerritRepo: Repository
+): Promise<GitReviewFile | null> {
+	log('Inferring host and project from remote');
+	const { success, stdout } = await tryExecAsync(
+		'git rev-parse --abbrev-ref --symbolic-full-name @{u}',
+		{
+			cwd: gerritRepo.rootUri.fsPath,
+		}
+	);
+	if (!success) {
+		log('Failed to get remote');
+		return null;
+	}
+
+	const remote = stdout.trim().split('/')[0];
+	const { success: success2, stdout: stdout2 } = await tryExecAsync(
+		`git remote get-url ${remote}`,
+		{
+			cwd: gerritRepo.rootUri.fsPath,
+		}
+	);
+	if (!success2) {
+		log('Failed to get remote URL');
+		return null;
+	}
+
+	const remoteUrl = stdout2.trim();
+	const urlRegex =
+		/(?:ssh|http|https):\/\/(?:[^@]+@)?([^:/]+)(?::(\d+))?\/(.+?)(?:\.git)?$/;
+	const match = urlRegex.exec(remoteUrl);
+	if (match) {
+		const [, host, port, project] = match;
+		return {
+			host,
+			port: port || DEFAULT_GIT_REVIEW_FILE.port,
+			project,
+		};
+	} else {
+		log('Failed to parse remote URL');
+		return null;
+	}
+}
+
 function parseGerritFile(fileContent: string): GitReviewFile | null {
 	if (!fileContent.includes('[gerrit]')) {
 		// This header should be in there
@@ -39,13 +84,6 @@ function parseGerritFile(fileContent: string): GitReviewFile | null {
 		const [key, value] = line.split('=');
 		const typedKey = key as keyof GitReviewFile;
 		file[typedKey] = value.trim();
-	}
-
-	if (!file.host || !file.project) {
-		log(
-			'Found a `.gitreview` file but it did not contain a host and project which are required'
-		);
-		return null;
 	}
 
 	return file as GitReviewFile;
@@ -66,14 +104,23 @@ export async function getGitReviewFile(
 		}
 	})();
 	if (!fileContent) {
-		return null;
+		return await inferHostAndProject(gerritRepo);
 	}
 
 	const parsed = parseGerritFile(fileContent);
-	if (parsed) {
-		return parsed;
+	const hostAndProject = await inferHostAndProject(gerritRepo);
+	if (hostAndProject) {
+		return {
+			...hostAndProject,
+			...parsed,
+		};
 	}
-	return null;
+
+	if (!parsed?.host || !parsed?.project) {
+		return null;
+	}
+
+	return parsed;
 }
 
 export const getGitReviewFileCached = createCacheWrapper(getGitReviewFile);
